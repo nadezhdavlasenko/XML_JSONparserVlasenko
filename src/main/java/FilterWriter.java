3ff2ee5f19c55286d4e80a6ecf08ca6236689773
Nadezhda Vlasenko <nadezhda277135@gmail.com>
2018-09-30 14:20:19 +0300

    Write rich people to console and to file

--- src/main/java/FilterWriter.java
+++ src/main/java/FilterWriter.java
@@ -1,2 +1,40 @@
-public class FilterWriter {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FilterWriter<T> {
+
+    private static final FilterWriter filterWriter = new FilterWriter();
+
+    private FilterWriter() {
+    }
+
+    public static FilterWriter getFilterWriter() {
+        return filterWriter;
+    }
+
+    public void filterAnDWriteToFile(List<T> list, Predicate<T> predicate, String fileName) {
+
+        Path path = Paths.get(fileName);
+
+        //Use try-with-resource to get auto-closeable writer instance
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            for (T t : list) {
+
+
+                if (predicate.test(t))
+                    writer.write(t.toString() + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void filterAnDWriteToConsole(List<T> list, Predicate<T> predicate) {
+        list.stream().filter(predicate).forEach(System.out::println);
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,11 @@ import javax.xml.transform.TransformerConfigurationException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static final String xmlFilePath = "xmlfile.xml";
+    public static final String richPeoplePath = "richPeople.txt";
 
     public static void main(String[] args) throws TransformerConfigurationException {
         List<Person> people = Arrays.asList(
@@ -16,6 +18,12 @@ public class Main {
 
         List<Person> peopleFromXML = XMLParser.parseRichPeople(xmlFilePath);
 
+        final int richBorder = 10000;
+        Predicate<Person> predicate = x->x.getCash()> richBorder;
+
+        FilterWriter filterWriter = FilterWriter.getFilterWriter();
+        filterWriter.filterAnDWriteToFile(peopleFromXML, predicate, richPeoplePath);
+        filterWriter.filterAnDWriteToConsole(peopleFromXML,predicate);
         //richPeople.stream().forEach(System.out::println);
 
 
