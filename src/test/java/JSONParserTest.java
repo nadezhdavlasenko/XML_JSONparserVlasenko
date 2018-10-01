62ec1f937074338fa52ab24f733ecfd17a230ed5
Nadezhda Vlasenko <nadezhda277135@gmail.com>
2018-10-01 21:31:14 +0300

    Test getJSONStringFromAPI()

--- src/test/java/JSONParserTest.java
+++ src/test/java/JSONParserTest.java
@@ -1,16 +1,21 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.Spy;
 
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.*;
 
 import static junit.framework.Assert.assertNotSame;
 import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class JSONParserTest {
@@ -20,10 +25,26 @@ public class JSONParserTest {
 //    @Spy
    // BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
     JSONParser jsonParser = JSONParser.getJsonParser();
+    //@Spy
+
+    int responsecode;
 
     @Test
-    public void getJSONStringFromAPI(){
+    public void getJSONStringFromAPI() throws IOException {
+        final String url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+        String resultString = jsonParser.getJSONStringFromAPI(url);
+        assert (resultString.contains("\"txt\":\"Австралійський долар\","));
+    }
 
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void testGetJSONStringFromAPIThrowsException() throws IOException {
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("HttpResponseCode: 404");
+        final String url = "https://flippingbook.com/404";
+        String resultString = jsonParser.getJSONStringFromAPI(url);
     }
 
     @Test
